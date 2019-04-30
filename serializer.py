a583c89351e22ea42a14e9eb907614231815a1e3
Sami Kohvakka <sami.kohvakka@live.com>
2019-04-30 19:07:15 +0300

    Print output to stdout

--- serializer.py
+++ serializer.py
@@ -4,69 +4,112 @@ Created on Sun Jan 27 16:49:47 2019
 
 @author: Sami
 """
+N = 1000
 
 import json
-import pickle
+try:
+    import pickle
+except ModuleNotFoundError:
+    print("Install pickle first using pip install pickle\n")
+    exit(0)
+
 import time
-import yaml
-import xmltodict
-import msgpack
+
+try:
+    import yaml
+except ModuleNotFoundError:
+    print("Install PyYAML first using pip install PyYAML\n")
+    exit(0)
+try:
+    import xmltodict
+except ModuleNotFoundError:
+    print("Install xmltodict first using pip install xmltodict\n")
+    exit(0)
+try:
+    import msgpack
+except ModuleNotFoundError:
+    print("Install msgpack first using pip install msgpack\n")
+    exit(0)
+
 
 o = {
- "string": "This is a string",
- "integer": 51234,
- "float": 6.324234,
- "array": ["a", "b", "c"],
- "object": {"prop": "value", "arr": [1,2,3]},
- }
+    "root": {
+        "string": "This is a string",
+        "integer": 51234,
+        "float": 6.324234,
+        "array": ["a", "b", "c"],
+        "object": {
+            "prop": "value",
+            "arr": [1, 2, 3]
+        },
+
+    }
+}
 
 times = {}
 
 # Native
 
 start = time.time()
-serialized = pickle.dumps(o)
+for i in range(0,N):
+    serialized = pickle.dumps(o)
 end1  = time.time()
-deserialized = pickle.loads(serialized)
+for i in range(0,N):
+    deserialized = pickle.loads(serialized)
 end2 = time.time()
-times["native"] = {"serialization": end1 - start, "deserialization": end2 - end1}
+times["native"] = {"serialization": (end1 - start) / N, "deserialization": (end2 - end1) / N}
 
 # XML
 start = time.time()
-serialized = xmltodict.unparse(o, pretty=True)
-print(serialized)
+for i in range(0,N):
+    serialized = xmltodict.unparse(o, pretty=True)
 end1  = time.time()
-deserialized = xmltodict.parse(serialized)
+for i in range(0,N):
+    deserialized = xmltodict.parse(serialized)
 end2 = time.time()
-times["xml"] = {"serialization": end1 - start, "deserialization": end2 - end1}
+times["xml"] = {"serialization": (end1 - start) / N, "deserialization": (end2 - end1) / N}
 
 # JSON
 start = time.time()
-serialized = json.dumps(o)
+for i in range(0,N):
+    serialized = json.dumps(o)
 end1  = time.time()
-deserialized = json.loads(serialized)
+for i in range(0,N):
+    deserialized = json.loads(serialized)
 end2 = time.time()
-times["json"] = {"serialization": end1 - start, "deserialization": end2 - end1}
+times["json"] = {"serialization": (end1 - start) / N, "deserialization": (end2 - end1) / N}
 
 # MessagePack
 start = time.time()
-serialized = msgpack.packb(o, use_bin_type=True)
+for i in range(0,N):
+    serialized = msgpack.packb(o, use_bin_type=True)
 end1  = time.time()
-deserialized = msgpack.unpackb(serialized, raw=False)
-print(deserialized)
+for i in range(0,N):
+    deserialized = msgpack.unpackb(serialized, raw=False)
 end2 = time.time()
-times["msgpack"] = {"serialization": end1 - start, "deserialization": end2 - end1}
+times["msgpack"] = {"serialization": (end1 - start) / N, "deserialization": (end2 - end1) / N}
 
 
 # YAML
 start = time.time()
-serialized = yaml.dump(o)
+for i in range(0,N):
+    serialized = yaml.dump(o)
 end1  = time.time()
-deserialized = yaml.load(serialized)
+for i in range(0,N):
+    deserialized = yaml.load(serialized)
 end2 = time.time()
-times["yaml"] = {"serialization": end1 - start, "deserialization": end2 - end1}
+times["yaml"] = {"serialization": (end1 - start) / N, "deserialization": (end2 - end1) / N}
+
+out = json.dumps(times, indent=2)
+print(out)
+
+print(times.keys())
+print(times.values())
 
 # plot
 import matplotlib.pyplot as plt
 import numpy as np
 
+fig = plt.figure()
+
+plt.show()
